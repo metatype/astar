3b10e082359585dbc20c0c4009261dd77b1594f2
Anthony Baker <metatype@yahoo.com>
2013-10-26 09:22:00 -0700

    Improved formatting and comments

--- src/main/java/metatype/astar/AstarPathfinder.java
+++ src/main/java/metatype/astar/AstarPathfinder.java
@@ -8,9 +8,9 @@ import java.util.PriorityQueue;
 import java.util.Set;
 
 /**
- * Finds the shortest distance between two points using the A* search
- * algorithm.  Uses an heuristic estimate to improve the routing speed and prune
- * the search space.
+ * Finds the shortest distance between two points using the A* search algorithm.
+ * Uses an heuristic estimate to improve the routing speed and prune the search
+ * space.
  * 
  * @see http://en.wikipedia.org/wiki/A*_search_algorithm
  * 
@@ -35,9 +35,9 @@ public class AstarPathfinder<V, E> {
    * @return the edges comprising the path, or {} if no path exists
    */
   public Iterable<E> route(Graph<V, E> graph, V start, V end) {
-    Node goal = new AstarPathfinder<V, E>().findShortestPath(new Path(
-        graph, start, end));
-    
+    Node goal = new AstarPathfinder<V, E>().findShortestPath(new Path(graph,
+        start, end));
+
     LinkedList<E> path = new LinkedList<E>();
     if (goal != null) {
       while (goal.edge != null) {
@@ -48,45 +48,45 @@ public class AstarPathfinder<V, E> {
 
     return path;
   }
-  
-  private Node findShortestPath(Path r) {
+
+  private Node findShortestPath(Path p) {
     // track queue insertions to improve path selection
     int order = 0;
-    
+
     // nodes that have already been visited
     Set<Node> closed = new HashSet<Node>();
-    
+
     // nodes that are candidates for exploration
     PriorityQueue<Node> open = new PriorityQueue<Node>();
-    
-    Node n = r.getNode(r.start);
-    n.reset(null, null, 0, order++);
-    open.add(n);
-    
-    // the path cost
-    double g, f = 0;
-    
-    Node current = n;
+
+    // let's start at the very beginning
+    // a very good place to start
+    Node current = p.getNode(p.start);
+    current.reset(null, null, 0, order++);
+    open.add(current);
+
+    // examine the next most promising node as determined by the path cost
+    // estimate, f = g + h
     while (!open.isEmpty()) {
       current = open.remove();
-      
+
       // all done!
-      if (current.vertex == r.end) {
+      if (current.vertex == p.end) {
         return current;
       }
-      
-      closed.add(current);
-      for (E edge : r.graph.outboundEdges(current.vertex)) {
-        n = r.getNode(r.graph.getTarget(edge));
 
-        g = current.g + r.graph.getWeight(edge);
-        f = g + n.h;
-        
+      closed.add(current);
+      for (E edge : p.graph.outboundEdges(current.vertex)) {
+        Node n = p.getNode(p.graph.getTarget(edge));
+
+        double g = current.g + p.graph.getWeight(edge);
+        double f = g + n.h;
+
         // we already found a better path
         if (closed.contains(n) && f >= n.f()) {
           continue;
         }
-        
+
         // need to explore this alternative
         if (!open.contains(n) || f < n.f()) {
           open.remove(n);
@@ -116,28 +116,38 @@ public class AstarPathfinder<V, E> {
 
       nodes = new HashMap<V, Node>();
     }
-    
+
     private Node getNode(V vertex) {
       Node n = nodes.get(vertex);
       if (n == null) {
         double h = graph.getHeuristic().estimate(vertex, end);
         n = new Node(vertex, h);
-        
+
         nodes.put(vertex, n);
       }
       return n;
     }
   }
-  
+
   private class Node implements Comparable<Node> {
+    /** the enclosed vertex */
     private final V vertex;
+
+    /** the estimate to the goal */
     private final double h;
-    
+
+    /** the prev node on the path, or null if not set */
     private Node prev;
+
+    /** the edge leading from prev */
     private E edge;
+
+    /** the actual incurred path cost so far */
     private double g;
+
+    /** the insertion order */
     private int order;
-    
+
     public Node(V vertex, double h) {
       this.vertex = vertex;
       this.h = h;
@@ -149,16 +159,21 @@ public class AstarPathfinder<V, E> {
       this.g = g;
       this.order = order;
     }
-    
+
+    /**
+     * Returns the path cost estimate, g + h.
+     * 
+     * @return the path length
+     */
     private double f() {
       return g + h;
     }
-    
+
     @Override
     public String toString() {
       return String.format("%s/%f", vertex, h);
     }
-    
+
     @Override
     public int compareTo(Node o) {
       if (this == o) {
@@ -167,12 +182,12 @@ public class AstarPathfinder<V, E> {
 
       // compare total estimated cost
       int diff = (int) Math.signum(f() - o.f());
-      
+
       // prefer shortest remaining cost
       if (diff == 0) {
         diff = (int) Math.signum(h - o.h);
       }
-      
+
       // prefer most recently inserted
       if (diff == 0) {
         diff = order = o.order;
